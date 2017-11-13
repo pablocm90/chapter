bbd72d885cae3d077aec1635225ca5d04b6a4fc8
Lynn Vanderhallen <lynn.vanderhallen@gmail.com>
2017-11-13 16:16:10 +0100

    initial show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,7 @@
 class BooksController < ApplicationController
+  # The show method displays only further details on a single book
+  # Find only the book that has the id defined in params[:id]
+  def show
+     @book = Book.find(params[:id])
+  end
 end
