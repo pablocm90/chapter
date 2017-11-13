fbb9db6e76483329a19ceea587e2f3ebc9925bc1
yoshi van roeyen <yoshivanroeyen@gmail.com>
2017-11-13 17:16:49 +0100

    temp changes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,7 +11,7 @@ class BooksController < ApplicationController
     Book.reindex
     @books = (params[:query].present?) ? Book.search(params[:query]) : Book.all
     # sort_results(@books)
-
+  end
   def index
     @books = Book.all
   end
