ac4d1934805def7155eab53b72b084228f5b278b
Lynn Vanderhallen <lynn.vanderhallen@gmail.com>
2017-11-13 17:16:20 +0100

    not finished on the style part

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,15 +2,15 @@ class BooksController < ApplicationController
   # The show method displays only further details on a single book
   # Find only the book that has the id defined in params[:id]
   def show
-     @book = Book.find(params[:id])
-  end
+   @book = Book.find(params[:id])
+ end
 
-
-  def search
+ def search
     # onder index te plaatsen
     Book.reindex
     @books = (params[:query].present?) ? Book.search(params[:query]) : Book.all
     # sort_results(@books)
+  end
 
   def index
     @books = Book.all
