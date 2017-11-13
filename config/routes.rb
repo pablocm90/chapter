18b2efa50844f1afbe81ebf6525f3e0a0f2471aa
yoshi van roeyen <yoshivanroeyen@gmail.com>
2017-11-13 16:14:20 +0100

    index on book controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resource :books
+
+  get 'books/index', to: 'books#index'
 end
