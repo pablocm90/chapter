179820e1abc90ad9dd585fcedc5017bd6f770c83
yoshi van roeyen <yoshivanroeyen@gmail.com>
2017-11-13 16:27:12 +0100

    fucked up seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,14 +6,13 @@ p "erasing everything"
 
 p "creating books"
 
-background-images = ['assets/img-1.jpg', 'assets/img-2.jpeg', 'assets/img-3.jpeg','assets/img-4.jpeg']
+
 
 10.times do
   params = {}
   params[:title] = [Faker::HowIMetYourMother.catch_phrase, Faker::HowIMetYourMother.high_five, Faker::Pokemon.move].sample
   params[:genre] = Faker::Book.genre
   params[:description] = Faker::StarWars.quote
-  params[:cover_pic] = background-images.sample
   book = Book.new(params)
   book.save
 end
