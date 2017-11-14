f9e1aa5d4c4cf010331ba9ff7694442d1104de42
pablo curell <pablocm90@gmail.com>
2017-11-14 11:58:00 +0100

    fixed seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
 require 'faker'
 
 p "erasing everything"
-
-  Book.destroy_all
+ Book.destroy_all
 
 p "creating books"
 
-genres_array = %w(fantasy scy-fi horror comedy crime thriler)
+# genres_array = %w(fantasy scy-fi horror comedy crime thriler)
 
 10.times do
+
   params = {}
   params[:title] = [Faker::HowIMetYourMother.catch_phrase, Faker::HowIMetYourMother.high_five, Faker::Pokemon.move].sample
-  params[:genre] = genres_array.sample
+  params[:genre] = Book::GENRES
   params[:description] = Faker::StarWars.quote
   params[:quote_hover] = Faker::Pokemon.move
   book = Book.new(params)
