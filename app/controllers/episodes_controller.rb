ab0fa8bbbe681a583e0ebb1e1f0b2166015f9da4
pablo curell <pablocm90@gmail.com>
2017-11-14 13:37:43 +0100

    initial view for chapters there

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,2 +1,7 @@
 class EpisodesController < ApplicationController
+
+  def index
+    @episodes = Episode.all
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   resources :books do
-    resources :chapters
+    resources :episodes
     collection do
       get 'search', to: 'books#search'
     end
