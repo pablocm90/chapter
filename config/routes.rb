6a25489c9f84e6f4b6e6893f8e33b23270c0c853
pablo curell <pablocm90@gmail.com>
2017-11-16 15:46:16 +0100

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
 Rails.application.routes.draw do
   devise_for :registrations
   root to: 'pages#home'
-  resources :books do
-    resources :episodes
+
+  resource :users, except: [ :new, :create, :index ] do
+    resources :transactions, only: [ :index ]
+  end
+
+  resource :author, except: [ :new, :create ] do
+    resources :books, except: [ :show, :index ]
+  end
+
+  resources :author, only: [ :new, :create ]
+
+  resources :books, only: [ :show ] do
     collection do
       get 'search', to: 'books#search'
     end
-    member do
-    get "buy", to: "books#buy"
+    resources :episodes, except: [:index] do
+      resources :reviews, except: [:destroy, :show]
     end
-    resources :reviews, except: [:destroy, :show]
-  end
-  get '/books/:book_id/episodes/:id/buy' => 'episodes#buy', as: :buy_episodes
-
-  resource :users, except: [ :index ] do
-    resources :transactions, only: [ :index ]
-  end
   get 'dashboard', to: 'users#dashboard'
+end
 end
